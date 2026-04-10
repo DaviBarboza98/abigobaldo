e8fd064ea9744b92de3ddace57b94a1e236ccdc9
Caaabooomm <gabriel.vitor.figueredo@gmail.com>
2026-04-10 13:52:21 -0300

    Sistema de Itens saBOr completo
    terminei pegar, arremessar e dropar

--- Abigobaldo/Assets/Scripts/Components/Itens/Item.cs
+++ Abigobaldo/Assets/Scripts/Components/Itens/Item.cs
@@ -4,15 +4,25 @@ public class Item : MonoBehaviour
 {
     public ItemType itemType;
     public ItemState currentState = ItemState.Cru;
+    [HideInInspector] public Transform followTarget;
 
     [Header("=== REFERENCIAS ===")]
     public Rigidbody rb;
     public Collider col;
 
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
+    }
+    void LateUpdate()
+    {
+        if (followTarget != null)
+        {
+            transform.position = followTarget.position;
+            transform.rotation = followTarget.rotation;
+        }
     }
 }
 
